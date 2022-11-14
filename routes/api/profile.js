58b31b911172143b1d85ddd125f9718b4415ac4c
Kaptan_Singh <kaptanpal123098@gmail.com>
2022-11-14 10:40:00 +0530

    Added Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -131,8 +131,11 @@ router.get('/user/:user_id', async (req, res) => {
     } catch(err) { 
         console.error(err.message);
 
+
+        // There is a Problem here, it is never entering in this if()
         if(err.kind == 'ObjectID'){
             console.log('here it comes--------');
+            
             return res.status(400).json({msg: 'profile not found'}); 
         }
         res.status(500).send('Server Error');   
@@ -204,5 +207,27 @@ router.put('/experience', [ auth, [
         res.status(400).send('Server Error'); 
     }
  });
+
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    DELETE experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req,res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id}); 
+
+        // Get the remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id); 
+
+        profile.experience.splice(removeIndex, 1); 
+
+        await profile.save(); 
+
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send('Server Error'); 
+    }
+})
 
 module.exports = router; 
